bc20ea29a6e5de89770d59a797adf57a4d88185d
svendietiker <sven.dietiker@gmail.com>
2026-09-22 14:24:38 +0200

    add new

--- src/main/java/ch/zhaw/avalanger/model/Avalange.java
+++ src/main/java/ch/zhaw/avalanger/model/Avalange.java
@@ -1,26 +1,15 @@
 package ch.zhaw.avalanger.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter 
+@AllArgsConstructor 
+
 public class Avalange {
     private String country;
     private String state;
     private String description;
 
-    public Avalange(String country, String state, String description) {
-        this.country = country;
-        this.state = state;
-        this.description = description;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-    
-    public String getState() {
-        return state;
-    }
-
-    public String getDescription() {
-        return description;
-    }
     
 }
